c61a43768635ea9e631bfc5456d4c25b412f2d46
Jamal Shareef <theepie@hotmail.com>
2019-04-10 22:58:39 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,5 +5,8 @@ def run_guessing_game
   input = gets.chomp
   if input.downcase == "exit"
     break
-  
+  if input.to_i < 1 || input.to_i > 6
+    puts "Please guess a number between 1 and 6"    
+  if input.to_i == rand(1..6)
+    puts 
 end
